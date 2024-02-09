2680c841fa637795afd0f6f9ab13cf3eb1d7de97
madhavitippani <madhavitippani28@gmail.com>
2024-02-08 19:58:57 -0500

    initiate bioformats reader and extract image sizes

--- code/VistoSeg/code/svs2tif.m
+++ code/VistoSeg/code/svs2tif.m
@@ -6,3 +6,9 @@ dt = '/dcs04/lieber/marmaypag/spatialAMY_LIBD4125/spatialAmygdala/raw-data/image
 ot = '/dcs04/lieber/marmaypag/spatialAMY_LIBD4125/spatialAmygdala/processed-data/Images/VistoSeg/';
 fname = 'V13Y24-346_40x.svs';
 % Open the .svs file using BioFormats
+reader = bfGetReader(fullfile(dt,fname));
+
+% Get image size
+sizeX = reader.getSizeX();
+sizeY = reader.getSizeY();
+numPlanes = reader.getImageCount();
