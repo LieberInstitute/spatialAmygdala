af1a2f033f85eb124fd4def5257b560bc4ef8e1b
Ryan Miller <ry.a.miller3@gmail.com>
2024-09-30 10:28:50 -0400

    spaceranger re-ran for the first donor in the project with logs for spaceranger run information

---
+++ code/01_spaceranger/03-spaceranger_first_donor.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+#SBATCH --mem=80G
+#SBATCH -n 8
+#SBATCH --job-name=amy-spaceranger
+#SBATCH -o logs/spaceranger-240926_%a.o.txt
+#SBATCH --array=1-8
+
+# 4%6
+
+echo "**** Job starts ****"
+date
+
+echo "**** JHPCE info ****"
+echo "User: ${USER}"
+echo "Job id: ${SLURM_JOBID}"
+echo "Job name: ${SLURM_JOB_NAME}"
+echo "Hostname: ${SLURM_NODENAME}"
+echo "Task id: ${SLURM_ARRAY_TASK_ID}"
+
+## load SpaceRanger
+module load spaceranger/3.0.0
+
+## List current modules for reproducibility
+module list
+
+## Locate file
+SAMPLE=$(awk "NR==${SLURM_ARRAY_TASK_ID}" 01-spaceranger_first_donor.txt)
+echo "Processing sample ${SAMPLE}"
+date
+
+## Get slide and area
+SLIDE=$(echo ${SAMPLE} | cut -d "_" -f 1)
+CAPTUREAREA=$(echo ${SAMPLE} | cut -d "_" -f 2)
+SAM=$(paste <(echo ${SLIDE}) <(echo "_") <(echo ${CAPTUREAREA}) -d '')
+echo "Slide: ${SLIDE}, capture area: ${CAPTUREAREA}"
+
+## Find FASTQ file path
+FASTQPATH=$(ls -d /dcs04/lieber/marmaypag/spatialAMY_LIBD4125/spatialAmygdala/raw-data/FASTQ/${SAMPLE}/)
+
+## Hank from 10x Genomics recommended setting this environment
+export NUMBA_NUM_THREADS=1
+
+## Run SpaceRanger
+spaceranger count \
+    --id=${SAMPLE} \
+    --transcriptome=/dcs04/lieber/lcolladotor/annotationFiles_LIBD001/10x/refdata-gex-GRCh38-2020-A \
+    --fastqs=${FASTQPATH}\
+    --image=/dcs04/lieber/marmaypag/spatialAMY_LIBD4125/spatialAmygdala/processed-data/Images/VistoSeg/${SAMPLE}.tif \
+    --slide=${SLIDE} \
+    --area=${CAPTUREAREA} \
+    --loupe-alignment=/dcs04/lieber/marmaypag/spatialAMY_LIBD4125/spatialAmygdala/processed-data/Images/loupe/${SAM}.json \
+    --jobmode=local \
+    --localcores=8 \
+    --localmem=64 \
+    --create-bam=false
+
+## Move output
+echo "Moving results to new location"
+date
+mkdir -p /dcs04/lieber/marmaypag/spatialAMY_LIBD4125/spatialAmygdala/processed-data/01_spaceranger/first_donor/
+mv ${SAMPLE} /dcs04/lieber/marmaypag/spatialAMY_LIBD4125/spatialAmygdala/processed-data/01_spaceranger/first_donor/
+
+echo "**** Job ends ****"
+date
+
+## This script was made using sgejobs version 0.99.1
+## available from http://research.libd.org/sgejobs/
+
+
+
+/dcs04/lieber/marmaypag/spatialAMY_LIBD4125/spatialAmygdala/raw-data/FASTQ/
